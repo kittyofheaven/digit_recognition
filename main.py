2eef5b468cd7cabffffddbfc68871b18e77ecb9b
shironeko <71081063+kittyofheaven@users.noreply.github.com>
2021-10-17 10:06:13 +0000

    impor numpy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 mnist = tf.keras.datasets.mnist
 (training_data, training_labels), (test_data, test_labels) = mnist.load_data()
@@ -9,6 +10,16 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 
-model.compile(optimizer=tf.train.AdamOptimizer(),
+model.compile(optimizer=tf.compat.v1.train.AdamOptimizer(),
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
+
+
+model.fit(training_data, training_labels, epochs=5)
+
+model.evaluate(test_data, test_labels)
+
+predictions = model.predict(test_data)
+np.set_printoptions(suppress=True)
+print(test_labels[0])
+print(predictions[0])
