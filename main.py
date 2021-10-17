deed02d46d3326954c1b515f5ea2478b04aa3fb3
shironeko <71081063+kittyofheaven@users.noreply.github.com>
2021-10-17 10:13:18 +0000

    add some comments

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ training_data, test_data = training_data / 255, test_data / 255
 model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
     tf.keras.layers.Dense(128, activation=tf.nn.relu), 
-    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax) # use softmax so it doesnt give strange output
 ])
 
 model.compile(optimizer=tf.compat.v1.train.AdamOptimizer(),
@@ -17,9 +17,9 @@ model.compile(optimizer=tf.compat.v1.train.AdamOptimizer(),
 
 
 model.fit(training_data, training_labels, epochs=5)
-
 model.evaluate(test_data, test_labels)
 
+# test mode accuracy for new data
 predictions = model.predict(test_data)
 np.set_printoptions(suppress=True)
 print(test_labels[0])
