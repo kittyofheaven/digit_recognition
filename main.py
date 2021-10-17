92d9ca396905f256cb6e29658dec2b7f7638652d
shironeko <71081063+kittyofheaven@users.noreply.github.com>
2021-10-17 10:03:32 +0000

    add neuron layers

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ import tensorflow as tf
 mnist = tf.keras.datasets.mnist
 (training_data, training_labels), (test_data, test_labels) = mnist.load_data()
 
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
